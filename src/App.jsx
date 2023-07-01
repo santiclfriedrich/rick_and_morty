7f5650d3e64193acc47c51943fc12ff3864a124d
santiclfriedrich <santiclear1@hotmail.com>
2023-06-30 22:18:15 -0300

    Cambion10 - Agrego de Login Form

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,33 @@ import Cards from './components/Cards/Cards.jsx';
 import Nav from './components/Nav/Nav.jsx';
 import About from './components/About/About.jsx';
 import Detail from './components/Detail/Detail.jsx';
+import Form from './components/Form/Form';
 import axios from 'axios';
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import NotFoundPage from './components/Error404/Error404';
+
+const email = 'santi@gmail.com';
+const password = '123456';
 
 function App() {
 
-   let [characters, setCharacters] = useState([])
+   const location = useLocation();
+   const navigate = useNavigate();
+   let [characters, setCharacters] = useState([]);
+   const [access, setAccess] = useState(false);
+
+   const login = (userData) => {
+
+      if(userData.email === email && userData.password === password){
+         setAccess(true);
+         navigate('/home');
+      }
+   }
+
+   useEffect(() => {
+      !access && navigate('/');
+   }, [access]);
 
    function onSearch(id) {
       axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
@@ -30,10 +49,16 @@ const onClose = (id) => {
 
    return (
       <div className='App'>
-         <Nav onSearch={onSearch} />
+         {
+            location.pathname !== '/' &&
+             <Nav onSearch={onSearch} />
+            
+         }
+         
          
 
          <Routes>
+            <Route path='/' element={<Form login={login} />} />
             <Route path='/home' element={<Cards characters={characters} onClose={onClose} />} />
             <Route path='/about' element={<About />} />
             <Route path='/detail/:id' element={<Detail />} />
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import validation from "../Validation/Validation"
+
+const Form = ({ login }) => {
+    const [errors, setErrors] = useState({})
+    const [userData, setUserData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (event) => {
+        setUserData({
+            ...userData,
+            [event.target.name]: event.target.value
+        })
+
+        setErrors(validation({
+            ...userData,
+            [event.target.name]: event.target.value
+        }))
+    }
+
+    const handleSubmit= (event) => {
+        event.preventDefault();
+        login(userData);
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+
+            <label htmlFor="email">Email:</label>
+            <input name="email" type="email" value={userData.email} onChange={handleChange} />
+            {errors.email && <p>{errors.email}</p>}
+
+            <label htmlFor="password">Contraseña</label>
+            <input name='password' type="password"  value={userData.password} onChange={handleChange} />
+            {errors.password && <p>{errors.password}</p>}
+
+            <button>Submit</button>
+
+        </form>
+    )
+}
+
+export default Form
