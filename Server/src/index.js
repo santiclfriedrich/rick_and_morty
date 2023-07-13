41fac48574120334c404f4a903e6d7ad1832d267
santiclfriedrich <santiclear1@hotmail.com>
2023-07-13 11:43:57 -0300

    Promesas agregadas al proyecto || hw3

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,23 +1,17 @@
 const http = require('http');
 const data = require('./utils/data')
+const { getCharById } = require('./controllers/getCharById');
 const PORT = 3001
 
 http
 .createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    console.log(req.url);
+   if(req.url.includes('/rickandmorty/character')){
+    const id = req.url.split('/').at(-1)
 
-    if (req.url.includes('/rickandmorty/character/')) {
-        const id = req.url.split('/').at(-1)
-        
-        const characterFound = data.find((character) => 
-          character.id === +id)
-
-        return res
-        .writeHead(200, { "Content-type": "application/json"})
-        .end(JSON.stringify(characterFound))
-    }
+    getCharById(res, +id);
+   }
 
 })
 .listen(PORT)
