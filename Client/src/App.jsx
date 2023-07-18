56caaf496d34e68e99fd34df1a3296ac5a6ed7db
santiclfriedrich <santiclear1@hotmail.com>
2023-07-17 23:37:36 -0300

    Async Await Changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -14,6 +14,8 @@ import NotFoundPage from './components/Error404/Error404';
 const email = 'santi@gmail.com';
 const password = '123456';
 
+const URL = 'http://localhost:3001/rickandmorty/login';
+
 function App() {
 
    const location = useLocation();
@@ -21,30 +23,41 @@ function App() {
    let [characters, setCharacters] = useState([]);
    const [access, setAccess] = useState(false);
 
-   const login = (userData) => {
+   const login = async (userData) => {
+      try {
+
       const { email, password } = userData;
-      const URL = 'http://localhost:3001/rickandmorty/login/';
-      axios(URL + `?email=${email}&password=${password}`)
-      .then(({ data }) => {
-         const { access } = data;
+      const { data } = await axios(URL + `?email=${email}&password=${password}`)
+      const { access } = data;
+
          setAccess(access);
          access && navigate('/home');
-      });
+
+      } catch (error) {
+         console.log(error.message);
+      }
+      
    }
 
    useEffect(() => {
       !access && navigate('/');
    }, [access]);
 
-   function onSearch(id) {
-      axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
+   const onSearch = async (id) => {
+      try {
+         const {data} = await axios(`http://localhost:3001/rickandmorty/character/${id}`)
+      
+
          if (data.name) {
             setCharacters((oldChars) => [...oldChars, data]);
             
-         } else {
-            alert('¡No hay personajes con este ID!');
-         }
-      });
+         } 
+         
+      } catch (error) {
+         alert('¡No hay personajes con este ID!');
+      }
+
+      
    }
 
 const onClose = (id) => {
